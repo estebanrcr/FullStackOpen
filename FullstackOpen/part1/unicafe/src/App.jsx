134444a35cbcf942c97bc692f81644b94cc4c77b
Esteban Costilla Rozzi <rozziesteban@gmail.com>
2024-02-15 08:58:08 -0300

    unicafe 1.7 paso 2

--- FullstackOpen/part1/unicafe/src/App.jsx
+++ FullstackOpen/part1/unicafe/src/App.jsx
@@ -1,12 +1,46 @@
 import { useState } from 'react'
 
 
+
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+const total = bad + good + neutral;
+
+
+const All = ({good, neutral, bad}) => {
+  return (
+  <li>
+    all: {good + bad + neutral}
+  </li>
+  )
+}
+const Positive = ({total, good}) => {
+  const percentage = good * 100 / total
+   
+   
+  return (
+  <li>
+    poitive: {isNaN(percentage) ? 0 : percentage}%
+  </li>
+  )
+}
+
+const Average = ({good, neutral, bad}) => {
+  const total = good + neutral + bad; 
+  if (total === 0) {
+    return <li>average: 0</li>
+  }
+  const average = (good - bad / total) / total;
+  return(
+    <li>
+      average: {average.toFixed(2)}
+    </li>
+  )
+}
 
   return (
     <>
@@ -22,6 +56,10 @@ const App = () => {
           <li>good: {good}</li>
           <li>neutral: {neutral}</li>
           <li>bad: {bad}</li>
+          <All good={good} neutral={neutral} bad={bad} />
+          <Average good={good} neutral={neutral} bad={bad} />
+          <Positive total={total} good={good}/>
+          
         </ul>
         
 
