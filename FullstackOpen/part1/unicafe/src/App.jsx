af0856aafcc9763b7f5f389f77168c52767c17f8
Esteban Costilla Rozzi <rozziesteban@gmail.com>
2024-02-15 09:34:20 -0300

    Unicafe 1.8 Paso 3

--- FullstackOpen/part1/unicafe/src/App.jsx
+++ FullstackOpen/part1/unicafe/src/App.jsx
@@ -1,46 +1,41 @@
 import { useState } from 'react'
 
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  
+  const Positive = () => {
+    const percentage = Math.round(good * 100 / total);
+    return <li>positive: {isNaN(percentage) ? 0 : percentage}%</li>;
+  };
+
+  const Average = () => {
+    if (total === 0) {
+      return <li>average: 0</li>
+    }
+    const average = (good - bad) / total;
+    return <li>average: {average.toFixed(2)}</li>
+  };
+
+  return (
+    <>
+    <h1>Statistics</h1>
+    <ul>
+      <li>good: {good}</li>
+      <li>neutral: {neutral}</li>
+      <li>bad: {bad}</li>
+      <li>all:{total}</li>
+      <Average />
+      <Positive />
+    </ul>
+    </>
+  )
+}
 
 const App = () => {
-
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-const total = bad + good + neutral;
-
-
-const All = ({good, neutral, bad}) => {
-  return (
-  <li>
-    all: {good + bad + neutral}
-  </li>
-  )
-}
-const Positive = ({total, good}) => {
-  const percentage = good * 100 / total
-   
-   
-  return (
-  <li>
-    poitive: {isNaN(percentage) ? 0 : percentage}%
-  </li>
-  )
-}
-
-const Average = ({good, neutral, bad}) => {
-  const total = good + neutral + bad; 
-  if (total === 0) {
-    return <li>average: 0</li>
-  }
-  const average = (good - bad / total) / total;
-  return(
-    <li>
-      average: {average.toFixed(2)}
-    </li>
-  )
-}
 
   return (
     <>
@@ -48,24 +43,9 @@ const Average = ({good, neutral, bad}) => {
         <h1>give feedback</h1>
         <button onClick={() => setGood(good+1)}>good</button>
         <button onClick={() => {setNeutral(neutral +1)}}>neutral</button>
-        <button onClick={() =>{setBad(bad+1)}}>bad</button>
-
-
-        <h1>statistics</h1>
-        <ul>
-          <li>good: {good}</li>
-          <li>neutral: {neutral}</li>
-          <li>bad: {bad}</li>
-          <All good={good} neutral={neutral} bad={bad} />
-          <Average good={good} neutral={neutral} bad={bad} />
-          <Positive total={total} good={good}/>
-          
-        </ul>
-        
-
-
-        
+        <button onClick={() =>{setBad(bad+1)}}>bad</button>       
       </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
